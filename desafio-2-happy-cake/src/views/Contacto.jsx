f5ce8baf4f692a1e5ca50d7a9fb3ae3955896f51
Jorge Espinoza <jorge.esp.ram@gmail.com>
2024-02-18 22:39:43 -0300

    primer commit

---
+++ desafio-2-happy-cake/src/views/Contacto.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contacto = () => {
+  return (
+    <div>
+      <h1>Cuéntanos, ¿en qué te podemos ayudar?</h1>
+    </div>
+  );
+};
+
+export default Contacto;
---
+++ desafio-2-happy-cake/src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a Happy Cake</h1>
+      <p>El lugar de los pasteles felices</p>
+    </div>
+  );
+};
+
+export default Home;
